a029e6adc0f8e216ecaaea74492d03630d6b1581
MarcusVMC <marcus_vmc@outlook.com>
2023-10-18 21:19:06 -0300

    Desafio feito

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
+let XP = "10000"
 let nome = "Marcus Vinícius"
-console.log("oi")
+const Mensagem = "O heroi " + nome + " Está no nível "
+
+if(XP <= "1000")
+{
+  XP = "Ferro"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "1001" && XP <= "2000")
+{
+  XP = "Bronze"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "2001" && XP <= "5000")
+{
+  XP = "Prata"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "6001" && XP <= "7000")
+{
+  XP = "Ouro"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "7001" && XP <= "8000")
+{
+  XP = "Platina"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "8001" && XP <= "9000")
+{
+  XP = "Ascendente"
+    console.log(Mensagem + XP)
+}
+else if (XP >= "9001" && XP <= "10000")
+{
+  XP = "Imortal"
+    console.log(Mensagem + XP)
+}
+else (XP >= 10001)
+{
+  XP = "Radiante"
+  console.log(Mensagem + XP)
+}
